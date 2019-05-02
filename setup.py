cb0d753a0804ee10ff9b720592e303483e025322
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2019-05-02 17:40:26 +0200

    remove requirements from setup.py
    even though, -e git+git://.. links can be given in requirements
    files, this is not possible for the "install_requires" section
    in setup.py. Therefore this is removed, and the requirements
    have to be installed manually until either diggrtoolbox is
    available via PyPI or I found out how to achieve that.

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup
 from os.path import abspath, dirname, join
 
-base_path = dirname(abspath(__file__))
-
-with open(join(base_path, "requirements.txt")) as requirements_file:
-    requirements = requirements_file.readlines()
-
 setup(
     name="daft",
     version="0.1",
     packages=["daft", "daft.fetcher", "daft.reader"],
-    install_requires=requirements,
     entry_points="""
         [console_scripts]
         daft=daft.tool:cli
