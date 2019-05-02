3226c84bc993e308fe89061514b882b110a5634a
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2019-05-02 15:06:10 +0200

    fix requirements and setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
+from os.path import abspath, dirname, join
+
+base_path = dirname(abspath(__file__))
+
+with open(join(base_path, "requirements.txt")) as requirements_file:
+    requirements = requirements_file.readlines()
 
 setup(
     name="daft",
     version="0.1",
     packages=["daft", "daft.fetcher", "daft.reader"],
-    install_requires=[
-        "SPARQLWrapper",
-        "Click", 
-        "requests", 
-        "provit", 
-        "pandas",
-        "PyYAML"],
+    install_requires=requirements,
     entry_points="""
         [console_scripts]
         daft=daft.tool:cli
